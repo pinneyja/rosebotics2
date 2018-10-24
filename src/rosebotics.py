04e6a2585631f4276b8d0152bc5a0bfc0222a118
pinneyja <pinneyja@rose-hulman.edu>
2018-10-24 10:59:19 -0400

    wrote class for color sensor

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -192,7 +192,7 @@ class Camera(object):
 
 
 class ColorSensor(rb.ColorSensor):
-    """ Primary author of this class:  PUT_YOUR_NAME_HERE. """
+    """ Primary author of this class:  Jacob Pinney. """
 
     def __init__(self, port=ev3.INPUT_3):
         super().__init__(port)
@@ -203,7 +203,9 @@ class ColorSensor(rb.ColorSensor):
         light intensity is less than the given value (threshold), which should
         be between 0 (no light reflected) and 100 (maximum light reflected).
         """
-        # TODO.
+        while True:
+            if self.get_reflected_intensity() < reflected_light_intensity:
+                return
 
     def wait_until_intensity_is_greater_than(self, reflected_light_intensity):
         """
@@ -211,7 +213,9 @@ class ColorSensor(rb.ColorSensor):
         light intensity is greater than the given value (threshold), which
         should be between 0 (no light reflected) and 100 (max light reflected).
         """
-        # TODO.
+        while True:
+            if self.get_reflected_intensity() > reflected_light_intensity:
+                return
 
     def wait_until_color_is(self, color):
         """
@@ -219,7 +223,9 @@ class ColorSensor(rb.ColorSensor):
         of what color it sees is the given color.
         The given color must be a Color (as defined above).
         """
-        # TODO.
+        while True:
+            if self.get_color() == color:
+                return
 
     def wait_until_color_is_one_of(self, colors):
         """
@@ -227,7 +233,10 @@ class ColorSensor(rb.ColorSensor):
         of what color it sees is any one of the given sequence of colors.
         Each item in the sequence must be a Color (as defined above).
         """
-        # TODO.
+        while True:
+            for color in colors:
+                if self.get_color() == color:
+                    return
 
 
 class InfraredSensorAsProximitySensor(object):
