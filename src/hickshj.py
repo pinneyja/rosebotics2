93a562ead0a4ff410d6d0b2d94f086a0ecc5573e
pinneyja <pinneyja@rose-hulman.edu>
2018-10-25 10:37:07 -0400

    returned hunter's file to original state

--- src/hickshj.py
+++ src/hickshj.py
@@ -9,14 +9,13 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
-    q = True
     robot = rb.Snatch3rRobot()
     robot.drive_system.left_wheel.reset_degrees_spun()
-    robot.drive_system.left_wheel.start_spinning(100)
-    robot.drive_system.right_wheel.start_spinning(100)
-    while q is True:
+    robot.drive_system.left_wheel.start_spinning()
+    robot.drive_system.right_wheel.start_spinning()
+    while True:
         if robot.drive_system.left_wheel.get_degrees_spun() >= 100:
-            q = False
+            break
     robot.drive_system.left_wheel.stop_spinning()
     robot.drive_system.right_wheel.stop_spinning()
 
