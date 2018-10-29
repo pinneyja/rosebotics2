2f0bcac86c6e940f3a224c86867145dc9fb477fe
mcgeeml <mcgeeml@rose-hulman.edu>
2018-10-28 22:56:23 -0400

    worked on black line method

--- src/mcgeeml.py
+++ src/mcgeeml.py
@@ -10,6 +10,17 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
     robot = rb.Snatch3rRobot()
+    color1 = rb.Color.BROWN.value
+    find_color(color1, robot)
+
+
+def find_color(color, robo):
+    robo.drive_system.start_moving()
+    while True:
+        if robo.color_sensor.get_color() == color:
+            robo.drive_system.stop_moving(stop_action='brake')
+            break
+
 
 
 main()
