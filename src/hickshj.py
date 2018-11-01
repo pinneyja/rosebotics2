98dd97d4ba18771570dcfbe5ddf12e14c0ab681f
hickshj <hickshj@rose-hulman.edu>
2018-11-01 10:58:50 -0400

    writing ngon

--- src/hickshj.py
+++ src/hickshj.py
@@ -1,27 +1,27 @@
 """
-  Capstone Project.  Code written by PUT_YOUR_NAME_HERE.
+  Capstone Project.  Code written by Hunter Hicks.
   Fall term, 2018-2019.
 """
 
 import rosebotics_new as rb
-import time
+import ev3dev.ev3 as ev3
 
 
 def main():
     """ Runs YOUR specific part of the project """
-    run_test_ngon()
+    #    run_test_ngon()
+    distance_beep()
 
 
 def run_test_ngon():
-    #    print('should make triangle')
-    #    ngon(3, 10)
+    print('should make triangle')
+    ngon(3, 10)
 
     print('should make square')
     ngon(4, 10)
 
-
-#    print('should be pentagon')
-#    ngon(5, 5)
+    print('should be pentagon')
+    ngon(5, 5)
 
 
 def ngon(sides, side_length):
@@ -30,6 +30,16 @@ def ngon(sides, side_length):
     for k in range(sides):
         bot.drive_system.go_straight_inches(side_length)
         bot.drive_system.spin_in_place_degrees(degrees)
+
+
+def distance_beep():
+    bot = rb.Snatch3rRobot
+    sensor = rb.InfraredAsProximitySensor(4)
+    while True:
+        d = sensor.get_distance_to_nearest_object_in_inches()
+        print(d)
+        if 11 <= d <= 13:
+            ev3.Sound.beep().wait()
 
 
 main()
