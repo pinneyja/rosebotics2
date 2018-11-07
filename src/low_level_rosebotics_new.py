631f3c42b6fec5addf44eaef917378faaa3bdbb5
pinneyja <pinneyja@rose-hulman.edu>
2018-11-07 10:56:51 -0500

    changed rosebotics_new to work properly again

--- src/low_level_rosebotics_new.py
+++ src/low_level_rosebotics_new.py
@@ -66,7 +66,7 @@ class Wheel(object):
         """
         if stop_action is None:
             stop_action = self.default_stop_action
-        self.motor.stop(stop_action=stop_action)
+        self.motor.stop(stop_action=ev3.Motor.STOP_ACTION_BRAKE)
 
     def get_degrees_spun(self):
         return self.motor.position
