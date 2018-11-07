733033aeb230546f789ab17e3dd78750be820a23
pinneyja <pinneyja@rose-hulman.edu>
2018-11-07 00:04:35 -0500

    changes to pinneyja

--- src/pinneyja.py
+++ src/pinneyja.py
@@ -5,6 +5,7 @@
 
 import rosebotics_new as rb
 import time
+import random
 import rosegraphics as rg
 import math
 
@@ -15,8 +16,10 @@ def main():
     run_test_arm_claw = False
     run_test_arm_claw_advanced = False
     run_test_final_project = False
+    run_localized_final_tests = True
 
-    robot = rb.Snatch3rRobot()
+    if False:
+        robot = rb.Snatch3rRobot()
 
     # Test follow black line function
     if run_test_black_line:
@@ -37,6 +40,16 @@ def main():
     if run_test_final_project:
         run_final_project(robot)
 
+    if run_localized_final_tests:
+        draw_sonar_image(random_method())
+
+
+def random_method():
+    seq = []
+    for k in range(0,360,10):
+        seq.append([k,random.randint(20,30)])
+    return seq
+
 
 def run_final_project(robot):
     results = perform_sonar_scan(robot)
@@ -49,26 +62,38 @@ def perform_sonar_scan(robot):
 
 
 def draw_sonar_image(seq_of_seq):
+    # 96 pixels in 1 inch
+    x_width = 700  # width of RoseGraphics window
+    y_height = 700  # height of RoseGraphics window
+    scale = 10  # scale of real inches to screen inches; if 10, then 5 real inches translates to .5 screen inches
+    window = rg.RoseWindow(x_width, y_height)
+    # p_origin = rg.Point(500, 500)
+    p_origin = rg.Point(x_width/2, y_height/2)
+    p_origin.fill_color = "red"
+    p_origin.outline_thickness = 10
+    p_origin.attach_to(window)
     points = []
     for seq in seq_of_seq:
-        degrees = seq[0]
-        distance = seq[1]
-        x_position = distance * math.cos(degrees)
-        y_position = distance * math.sin(degrees)
+        degrees = (seq[0] / 360) * 2 * math.pi
+        distance = (seq[1] * 96) / scale
+        x_position = (x_width / 2) + distance * math.cos(degrees)
+        y_position = (y_height / 2) + distance * math.sin(degrees)
         point = rg.Point(x_position, y_position)
         points.append(point)
-    window = rg.RoseWindow()
     for point in points:
+        point.outline_thickness = 5
         point.attach_to(window)
         window.render()
     for k in range(len(points)):
-        if k == len(points):
+        if k == len(points) - 1:
             line = rg.Line(points[k], points[0])
         else:
             line = rg.Line(points[k], points[k+1])
+        line.color = "gray"
         line.attach_to(window)
         window.render()
-    # TODO loop through sequence of points and draw them and lines between them
+    window.close_on_mouse_click()
+
 
 def follow_black_line(robot, is_counterclockwise):
     if is_counterclockwise:
